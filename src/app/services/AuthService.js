b0df0e04798144333665c114406413a489607a82
Ruanderson Vieira <ruanderson.vieira@gmail.com>
2019-07-17 11:49:03 -0300

    :shirt: chager JWT to TOKEN

--- src/app/services/AuthService.js
+++ src/app/services/AuthService.js
@@ -3,19 +3,19 @@ const crypt = require('crypto');
 const AuthService = (socket) => {
     socket.on('auth.login', function (data) {
         data = JSON.parse(data);
-        toHash = data.user + '.' + data.serviceInfo + '.' + process.env.JWT_ENCRYPTION;
+        toHash = data.user + '.' + data.serviceInfo + '.' + process.env.TOKEN_ENCRYPTION;
         const token = crypt.createHash('md5').update(toHash).digest('hex');
         socket.emit('auth.token', { token });
     });
     socket.on('auth.verify', function (data) {
         data = JSON.parse(data);
-        toHash = data.user + '.' + data.serviceInfo + '.' + process.env.JWT_ENCRYPTION;
+        toHash = data.user + '.' + data.serviceInfo + '.' + process.env.TOKEN_ENCRYPTION;
         const token = crypt.createHash('md5').update(toHash).digest('hex');
         if (data.token !== token) {
             return socket.disconnect();
         }else {
             socket.auth = true;
-            console.log(socket.auth);
+            // console.log(socket.auth);
         }
     });
 
